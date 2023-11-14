ff97443f23db23e69d41187c74746dd82f76ec25
“Ibtisam <ibtisam.arif@casculate.com>
2023-11-14 16:06:43 +0100

    timeout incrases

--- bot.js
+++ bot.js
@@ -1,51 +1,46 @@
-const axios = require('axios');
-const { load } = require('cheerio');
-const Slimbot = require('slimbot');
+const axios = require("axios");
+const { load } = require("cheerio");
+const Slimbot = require("slimbot");
 
-const bot = new Slimbot(
-  '6394316552:AAF9epm1nJxMwkrriJJQ-X30B7Ejs0pzV6o',
-);
-const groupChatId = '-986446582';
-
+const bot = new Slimbot("6394316552:AAF9epm1nJxMwkrriJJQ-X30B7Ejs0pzV6o");
+const groupChatId = "-986446582";
+const httpClient = axios.create();
+httpClient.defaults.timeout = 30000;
 async function sendPeriodicMessage() {
-  console.log('Job Started');
+  console.log("Job Started");
   try {
-    const html = await axios.get(
-      'https://service2.diplo.de/rktermin/extern/appointment_showForm.do?locationCode=isla&realmId=108&categoryId=1600',
+    const html = await httpClient.get(
+      "https://service2.diplo.de/rktermin/extern/appointment_showForm.do?locationCode=isla&realmId=108&categoryId=1600"
     );
+    console.log(html.data);
     const $ = load(html.data);
     $(`#appointment_newAppointmentForm_fields_3__content`)
-      .children('option')
+      .children("option")
       .each((_, elem) => {
-        if (elem.attribs['value'].toString().includes('2024')) {
-          const option = elem.attribs['value'].split('/');
+        if (elem.attribs["value"].toString().includes("2024")) {
+          const option = elem.attribs["value"].split("/");
           bot.sendMessage(
             groupChatId,
-            '----------------------------------------------------',
+            "----------------------------------------------------"
           );
           bot.sendMessage(
             groupChatId,
-            `Appointment found Keyword:2024 ${option[1]}`,
+            `Appointment found Keyword:2024 ${option[1]}`
           );
           bot.sendMessage(
             groupChatId,
-            '----------------------------------------------------',
+            "----------------------------------------------------"
           );
         }
       });
     bot.sendMessage(
       groupChatId,
-      `[Info] - ${JSON.stringify(
-        new Date(),
-      )} - Successfully scrapped!`,
+      `[Info] - ${JSON.stringify(new Date())} - Successfully scrapped!`
     );
   } catch (error) {
-    bot.sendMessage(
-      groupChatId,
-      `[Error] - ${JSON.stringify(error.message)}`,
-    );
+    bot.sendMessage(groupChatId, `[Error] - ${JSON.stringify(error.message)}`);
   }
-  console.log('Job Ended');
+  console.log("Job Ended");
 }
 
-module.exports = { sendPeriodicMessage }
+module.exports = { sendPeriodicMessage };
