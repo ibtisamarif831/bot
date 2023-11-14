3fe7b3145ed1f8038f6bf89e3205768a2b080093
“Ibtisam <ibtisam.arif@casculate.com>
2023-11-14 15:36:48 +0100

    added 0.0..00

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.listen(3000, "0.0.0.0");
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
 setInterval(bot.sendPeriodicMessage, interval);
