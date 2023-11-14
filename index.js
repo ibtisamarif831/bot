c83ae33f0e84b2112c08acb383f0b90335396798
“Ibtisam <ibtisam.arif@casculate.com>
2023-11-14 15:35:18 +0100

    added server

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-const bot = require('./bot');
-const interval = 1000 * 60 * 10
+const bot = require("./bot");
+const interval = 1000 * 60 * 5;
+const express = require("express");
+const app = express();
+const port = 3000;
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 setInterval(bot.sendPeriodicMessage, interval);
